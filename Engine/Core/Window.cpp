e4723a2bca9454245b67c68504efc7d2baeec9e0
Land Maze <61432929+Land-Maze@users.noreply.github.com>
2025-09-24 01:16:13 +0200

    [Core] Added window class, commented Vulkan example

---
+++ Engine/Core/Window.cpp
@@ -0,0 +1,126 @@
+#include "Window.h"
+
+#include <iostream>
+#include <ostream>
+
+namespace Core
+{
+    Window::Window()
+    {
+        if (!glfwInit())
+        {
+            throw std::runtime_error(
+                "Failed to initialize GLFW: glfwInit returned false. Is GLFW (for correct display manager) installed correctly?");
+        }
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+        _monitor = glfwGetPrimaryMonitor();
+        if (!_monitor)
+        {
+            throw std::runtime_error(
+                "Failed to get primary monitor: glfwGetPrimaryMonitor returned nullptr. Is monitor connected?");
+        }
+    }
+
+    Window::~Window()
+    {
+        close();
+        glfwTerminate();
+    }
+
+    void Window::close()
+    {
+        if (!_window) return;
+
+        glfwDestroyWindow(_window.release());
+    }
+
+    void Window::create(const WindowParams& window_params)
+    {
+        if (_window)
+        {
+            std::cout << "Window already created. Skipping create()" << std::endl;
+            return;
+        }
+
+        int count;
+        const auto modes = glfwGetVideoModes(_monitor, &count);
+
+
+        if (_isFullscreen)
+            _window = Smart_GLFWWindow(glfwCreateWindow(modes[0].width, modes[0].height, window_params.title.c_str(),
+                                                        _monitor, nullptr));
+        else
+            _window = Smart_GLFWWindow(glfwCreateWindow(window_params.width, window_params.height,
+                                                        window_params.title.c_str(),
+                                                        nullptr, nullptr));
+
+        if (!_window)
+            throw std::runtime_error(
+                "Unknown reason occurred during the window creation: glfwCreateWindow returned nullptr.");
+
+        _window_params = window_params;
+        setTitle(_window_params.title + " [DEBUG]");
+    }
+
+    void Window::pollEvents() const
+    {
+        if (!_window) return;
+
+        glfwPollEvents();
+    }
+
+    void Window::toggleFullscreen()
+    {
+        this->close();
+        _isFullscreen = !_isFullscreen;
+        this->create(_window_params);
+    }
+
+    bool Window::shouldClose() const
+    {
+        if (!_window) return false;
+
+        return glfwWindowShouldClose(_window.get());
+    }
+
+    void Window::setShouldClose(bool value) const
+    {
+        if (!_window) return;
+
+        glfwSetWindowShouldClose(_window.get(), value);
+    }
+
+    void Window::setTitle(const std::string& title)
+    {
+        _window_params.title = title;
+        if (!_window)
+            glfwSetWindowTitle(_window.get(), title.c_str());
+    }
+
+    bool Window::isRunning() const
+    {
+        return _window != nullptr;
+    }
+
+    void Window::setDimensions(int width, int height)
+    {
+        if (!_window) return;
+
+        glfwSetWindowSize(_window.get(), width, height);
+        _window_params.width = width;
+        _window_params.height = height;
+    }
+
+    std::pair<int, int> Window::getDimensions() const
+    {
+        return std::pair<int, int>{_window_params.width, _window_params.height};
+    }
+
+    GLFWwindow* Window::getGLFWWindow() const
+    {
+        return _window.get();
+    }
+}
---
+++ Engine/Core/Window.h
@@ -0,0 +1,134 @@
+#pragma once
+
+#include <memory>
+#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <optional>
+#include <GLFW/glfw3.h>
+
+#define WINDOW_16_10_RATIO 1.6f
+
+namespace Core
+{
+    struct DestroyGLFWWindow{
+        void operator()(GLFWwindow* ptr) const
+        {
+            if (ptr) glfwDestroyWindow(ptr);
+        }
+    };
+    typedef std::unique_ptr<GLFWwindow, DestroyGLFWWindow> Smart_GLFWWindow;
+
+    class Window
+    {
+    public:
+        Window();
+        ~Window();
+
+
+        /**
+         * @param width Width of the window (excluding DPI scaling)
+         * @param height of the window (excluding DPI scaling)
+         * @param title Title of the window
+         */
+        struct WindowParams
+        {
+            int width = 800;
+            int height = 600;
+            std::string title = "Window";
+
+        };
+
+        /**
+         * Creates a GLFW window with the specified title and dimensions.
+         *
+         * @param window_params Struct that includes title, width, height
+         */
+        void create(const WindowParams& window_params);
+
+        /**
+         * Polls events from the windowing system.
+         * Such as keyboard and mouse input, window resize, etc.
+         */
+        void pollEvents() const;
+
+        /**
+         * Toggles fullscreen mode for the window.
+         * It will delete the current window and create a new one in fullscreen or windowed mode.
+         */
+        void toggleFullscreen();
+
+        /**
+         * Checks if the window should close.
+         * For example, if the user has clicked the close button.
+         * Or if the application has requested to close the window (e.g. via ctrl+c or alt+f4).
+         *
+         * @return True if the window should close, False otherwise.
+         */
+        [[nodiscard]] bool shouldClose() const;
+
+        /**
+         * This function could be used to either request the window to close (e.g. user pressed Exit button)
+         * or to cancel a previous close request (e.g. user pressed Cancel button in a confirmation dialog).
+         *
+         * Still, it would not close the window immediately, but set a flag that would be checked in the main loop.
+         *
+         * @param value True to request the window to close, false to keep it open.
+         */
+        void setShouldClose(bool value) const;
+
+        /**
+         * Destroys the window and releases resources related to it.
+         * After calling this function, the window object should not be used anymore.
+         */
+        void close();
+
+        /**
+         *  Checks if the window is still running.
+         *
+         *  @return True if window is still running, False otherwise.
+         */
+        [[nodiscard]] bool isRunning() const;
+
+        /**
+         * Sets the title of the window.
+         *
+         * @param title New title for the window.
+         */
+        void setTitle(const std::string& title);
+
+        /**
+         * Sets the dimensions of the window.
+         * DPI scaling is taken into account internally, example:
+         * setDimensions(800, 600) on a 2x DPI scaling monitor will create a window of 1600x1200 actual pixels.
+         *
+         * @param width New width for the window in pixels (excluding DPI scaling).
+         * @param height New height for the window in pixels (excluding DPI scaling).
+         */
+        void setDimensions(int width, int height);
+
+        /**
+         *
+         * @return Returns a pointer to the underlying GLFW window object.
+         */
+        [[nodiscard]] GLFWwindow* getGLFWWindow() const;
+
+        /**
+         * Gets the dimensions of the window.
+         * DPI scaling is taken into account internally, example:
+         * setDimensions(800, 600) on a 2x DPI scaling monitor will create a window of 1600x1200 actual pixels.
+         *
+         * @return Pair of width and height
+         */
+        [[nodiscard]] std::pair<int, int> getDimensions() const;
+
+    private:
+        Smart_GLFWWindow _window = nullptr;
+        GLFWmonitor* _monitor = nullptr;
+
+        WindowParams _window_params;
+
+        float _monitorRatio = WINDOW_16_10_RATIO;
+
+        bool _isFullscreen = false;
+    };
+}
