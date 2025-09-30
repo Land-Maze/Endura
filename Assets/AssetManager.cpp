664db33b7cba4df86392404c8c7e3a4c66d38448
Timur Dzhesur <61432929+Land-Maze@users.noreply.github.com>
2025-10-01 01:55:54 +0200

    [ASSETS] Add scalable AssetManager

---
+++ Assets/AssetManager.cpp
@@ -0,0 +1,48 @@
+#include "AssetManager.h"
+
+#include <cstring>
+#include <fstream>
+
+namespace Assets
+{
+	template <>
+	std::shared_ptr<AssetType::Shader> AssetManager::load<AssetType::Shader>(const std::string& filename)
+	{
+		auto shader = std::make_shared<AssetType::Shader>();
+		auto file = openFile("Shader/" + filename);
+		auto fileSize = file.tellg();
+		std::vector<char> buffer(fileSize);
+
+		file.seekg(0, std::ios::beg);
+
+		file.read((buffer.data()),
+		          buffer.size());
+
+		shader->spirV.resize(fileSize / sizeof(uint32_t));
+
+		std::memcpy(shader->spirV.data(), buffer.data(), fileSize);
+
+		file.close();
+
+		return shader;
+	}
+
+	std::ifstream AssetManager::openFile(const std::string& filename)
+	{
+		std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+		if (!file.is_open())
+		{
+			throw std::runtime_error("failed to open file!");
+		}
+
+		return file;
+	}
+
+	template <typename T>
+	std::shared_ptr<T> AssetManager::load(const std::string& filename)
+	{
+		static_assert(sizeof(T) == 0, "Unsupported asset type in AssetManager::load");
+		return nullptr;
+	}
+}
---
+++ Assets/AssetManager.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <memory>
+#include <vector>
+
+namespace Assets
+{
+	namespace AssetType
+	{
+		struct Shader
+		{
+			std::vector<uint32_t> spirV;
+		};
+
+		struct Generic
+		{
+			std::vector<char> bytes;
+		};
+	}
+
+	class AssetManager
+	{
+	public:
+		template<typename T>
+		[[nodiscard]] std::shared_ptr<T> load(const std::string& filename);
+
+	private:
+		static std::ifstream openFile(const std::string& filename);
+	};
+}
