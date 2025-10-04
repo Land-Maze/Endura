3be237ed70d4b50943fe96cea15616779b270fa3
Timur Dzhesur <61432929+Land-Maze@users.noreply.github.com>
2025-10-04 19:47:32 +0200

    [CLIENT] Primitive FPS counter

--- Client/main.cpp
+++ Client/main.cpp
@@ -802,12 +802,27 @@ int main() {
     window.create({});
     vkContext.InitializeVulkan(window.getGLFWWindow());
 
+    uint32_t frames = 0;
+    float timer = 0.0f;
+
     while (true)
     {
         while (!window.shouldClose())
         {
+            const double timeStart = glfwGetTime();
+
             window.pollEvents();
             vkContext.drawFrame();
+            frames++;
+
+            timer += glfwGetTime() - timeStart;
+
+            if (timer >= 1.0f)
+            {
+                printf("FPS: %f\n", frames / timer);
+                timer = 0.0f;
+                frames = 0;
+            }
         }
         // We can have the exit logic here
         break;
